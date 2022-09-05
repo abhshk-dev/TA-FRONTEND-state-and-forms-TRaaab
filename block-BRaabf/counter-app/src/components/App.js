5901fbace509f3b5e75330866e8d4a0e3335a592
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-09-05 11:59:31 +0530

    final-push

--- block-BRaabf/counter-app/src/components/App.js
+++ block-BRaabf/counter-app/src/components/App.js
@@ -4,32 +4,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
+      count: 0,
+      step: 5,
+      max: 15,
     };
   }
   handleIncrement = () => {
     this.setState({
-      counter: this.state.counter + 1,
+      count:
+        this.state.count + this.state.step > this.state.max
+          ? this.state.count
+          : this.state.count + this.state.step,
     });
   };
   handleDecrement = () => {
     this.setState({
-      counter: this.state.counter - 1,
+      count: this.state.count - this.state.step,
     });
   };
   handleReset = () => {
     this.setState({
-      counter: 0,
+      count: 0,
     });
   };
   render() {
     return (
-      <div>
-        <h1>{this.state.counter}</h1>
+      <>
+        <h1>{this.state.count}</h1>
+        <div className="flex1">
+          <div>
+            {[5, 10, 15].map((step) => (
+              <button
+                onClick={() => this.setState({ step: step })}
+                className={this.state.step === step ? "active" : ""}
+              >
+                {step}
+              </button>
+            ))}
+          </div>
+          <div>
+            {[15, 100, 200].map((max) => (
+              <button
+                onClick={() => this.setState({ max: max })}
+                className={this.state.max === max ? "active" : ""}
+              >
+                {max}
+              </button>
+            ))}
+          </div>
+        </div>
         <button onClick={this.handleIncrement}>Increment</button>
         <button onClick={this.handleDecrement}>Decrement</button>
         <button onClick={this.handleReset}>Reset</button>
-      </div>
+      </>
     );
   }
 }
