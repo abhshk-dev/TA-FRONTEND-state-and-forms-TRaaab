00f78c9497e043f9c1be8b8ec8a86a3c5c04dda7
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-09-25 22:36:48 +0530

    final-push

---
+++ block-BRaabw/shopping-cart/src/components/CartItem.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <img src={`/static/products/${props.sku}_2.jpg`} alt="" width="80" />
+      <div className="cart-item-details">
+        <p className="cart-item-name">{props.title}</p>
+        <p>
+          {props.availableSizes[0]}|{props.style}
+        </p>
+        <p> Quantity: {props.quantity}</p>
+      </div>
+      <div className="cart-price">
+        <p className="cart-cross" onClick={() => props.deleteItem(props.id)}>
+          x
+        </p>
+        <p className="price">{`${props.currencyFormat + props.price}`}</p>
+        <div>
+          <Increment
+            incrementQuantity={() => props.incrementQuantity(props.id)}
+          />
+          <Decrement
+            decrementQuantity={() => props.decrementQuantity(props.id)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Increment(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="plus-icon"
+      onClick={props.incrementQuantity}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      />
+    </svg>
+  );
+}
+function Decrement(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="plus-icon"
+      onClick={props.decrementQuantity}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M18 12H6"
+      />
+    </svg>
+  );
+}
+
+export default CartItem;
---
+++ block-BRaabw/shopping-cart/src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Sidebar(props) {
+  let sizes = props.products.reduce((acc, cv) => {
+    acc = acc.concat(cv.availableSizes);
+    return acc;
+  }, []);
+  let uniqueSizes = [...new Set(sizes)]; // ['XS','S','L','XL','XXL']
+
+  let { selectedSizes } = props;
+
+  return (
+    <aside className="flex-20 sidebar">
+      <div className="flex wrap">
+        {uniqueSizes.map((size) => (
+          <span
+            key={size}
+            onClick={() => props.handleClick(size)}
+            className={`size ${selectedSizes.includes(size) ? "active" : ""}`}
+          >
+            {size}
+          </span>
+        ))}
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;
